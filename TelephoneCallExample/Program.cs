98adce2f0fb3cdd6687fb99f935c300654a726c7
Nicolas <Nicolas.Champanhac@apcurium.local>
2016-08-09 17:43:23 -0400

    add support or TArg

--- TelephoneCallExample/Program.cs
+++ TelephoneCallExample/Program.cs
@@ -1,92 +1,147 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
 using Stateless;
 
 namespace TelephoneCallExample
 {
     class Program
     {
+        static StateMachine<State, Trigger> driverStateMachine = new StateMachine<State, Trigger>(State.OffDuty);
+
         enum Trigger
         {
-            CallDialed,
-            HungUp,
-            CallConnected,
-            LeftMessage,
-            PlacedOnHold,
-            TakenOffHold,
-            PhoneHurledAgainstWall
+            ToDriving,
+            ToSleeperBerth,
+            ToOffDuty,
+            ToOnDutyNotDriving,
         }
 
         enum State
         {
-            OffHook,
-            Ringing,
-            Connected,
-            OnHold,
-            PhoneDestroyed
+            Driving,
+            SleeperBerth,
+            OffDuty,
+            OnDutyNotDriving
         }
 
         static void Main(string[] args)
         {
-            var phoneCall = new StateMachine<State, Trigger>(State.OffHook);
+            var toDriving = driverStateMachine.SetTriggerParameters<object>(Trigger.ToDriving);
+            var toSleeperBerth = driverStateMachine.SetTriggerParameters<object>(Trigger.ToSleeperBerth);
+            var toOnDutyNotDriving = driverStateMachine.SetTriggerParameters<object>(Trigger.ToOnDutyNotDriving);
+            var toOffDuty = driverStateMachine.SetTriggerParameters<object>(Trigger.ToOffDuty);
 
-            phoneCall.Configure(State.OffHook)
-	            .Permit(Trigger.CallDialed, State.Ringing);
-            	
-            phoneCall.Configure(State.Ringing)
-	            .Permit(Trigger.HungUp, State.OffHook)
-	            .Permit(Trigger.CallConnected, State.Connected);
-             
-            phoneCall.Configure(State.Connected)
-                .OnEntry(t => StartCallTimer())
-                .OnExit(t => StopCallTimer())
-	            .Permit(Trigger.LeftMessage, State.OffHook)
-	            .Permit(Trigger.HungUp, State.OffHook)
-	            .Permit(Trigger.PlacedOnHold, State.OnHold);
+            driverStateMachine.Configure(State.OffDuty)
+                .OnEntryFrom(toOffDuty, OffDutyOnEntry)
+                .OnExit(OffDutyOnExit)
+                .PermitDynamic(toDriving, _ => State.Driving)
+                .PermitDynamic(toSleeperBerth, _ => State.SleeperBerth)
+                .PermitDynamic(toOnDutyNotDriving, _ => State.OnDutyNotDriving);
 
-            phoneCall.Configure(State.OnHold)
-                .SubstateOf(State.Connected)
-                .Permit(Trigger.TakenOffHold, State.Connected)
-                .Permit(Trigger.HungUp, State.OffHook)
-                .Permit(Trigger.PhoneHurledAgainstWall, State.PhoneDestroyed);
+            driverStateMachine.Configure(State.SleeperBerth)
+                .OnEntryFrom(toSleeperBerth, SleeperBerthgOnEntry)
+                .OnExit(SleeperBerthgOnExit)
+                .PermitDynamic(toDriving, _ => State.Driving)
+                .PermitDynamic(toOffDuty, _ => State.OffDuty)
+                .PermitDynamic(toOnDutyNotDriving, _ => State.OnDutyNotDriving);
 
-            Print(phoneCall);
-            Fire(phoneCall, Trigger.CallDialed);
-            Print(phoneCall);
-            Fire(phoneCall, Trigger.CallConnected);
-            Print(phoneCall);
-            Fire(phoneCall, Trigger.PlacedOnHold);
-            Print(phoneCall);
-            Fire(phoneCall, Trigger.TakenOffHold);
-            Print(phoneCall);
-            Fire(phoneCall, Trigger.HungUp);
-            Print(phoneCall);
+            driverStateMachine.Configure(State.OnDutyNotDriving)
+                .OnEntryFrom(toOnDutyNotDriving, OnDutyNotDrivingOnEntry)
+                .OnExit(OnDutyNotDrivingOnExit)
+                .PermitDynamic(toOffDuty, _ => State.OffDuty)
+                .PermitDynamic(toSleeperBerth, _ => State.SleeperBerth)
+                .PermitDynamic(toDriving, _ => State.Driving);
+
+            driverStateMachine.Configure(State.Driving)
+                .OnEntryFrom(toDriving, DrivingOnEntry, "DrivingOnEntry")
+                .OnExit(DrivingOnExit)
+                .PermitDynamic(toOffDuty, _ => State.OffDuty)
+                .PermitDynamic(toSleeperBerth, _ => State.SleeperBerth)
+                .PermitDynamic(toOnDutyNotDriving, _ => State.OnDutyNotDriving);
+
+
+            Task.Run(() =>
+            {
+                Fire(driverStateMachine, Trigger.ToDriving, 1);
+                Fire(driverStateMachine, Trigger.ToOffDuty, 2);
+                Fire(driverStateMachine, Trigger.ToSleeperBerth, 3);
+                Fire(driverStateMachine, Trigger.ToOnDutyNotDriving, 4);
+                Fire(driverStateMachine, Trigger.ToOnDutyNotDriving, 5);
+                Fire(driverStateMachine, Trigger.ToDriving, 6);
+            });
+
+            Task.Run(() =>
+            {
+                Fire(driverStateMachine, Trigger.ToOffDuty, 7);
+                Fire(driverStateMachine, Trigger.ToSleeperBerth, 8);
+                Fire(driverStateMachine, Trigger.ToSleeperBerth, 9);
+                Fire(driverStateMachine, Trigger.ToOnDutyNotDriving, 10);
+            });
 
             Console.WriteLine("Press any key...");
             Console.ReadKey(true);
         }
 
-        static void StartCallTimer()
+        static void DrivingOnEntry(object value)
         {
-            Console.WriteLine("[Timer:] Call started at {0}", DateTime.Now);
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} DrivingOnEntry {value}");
         }
 
-        static void StopCallTimer()
+        static void DrivingOnExit()
         {
-            Console.WriteLine("[Timer:] Call ended at {0}", DateTime.Now);
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} DrivingOnExit");
         }
 
-        static void Fire(StateMachine<State, Trigger> phoneCall, Trigger trigger)
+        static void OnDutyNotDrivingOnEntry(object value)
         {
-            Console.WriteLine("[Firing:] {0}", trigger);
-            phoneCall.Fire(trigger);
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} OnDutyNotDrivingOnEntry {value}");
         }
 
-        static void Print(StateMachine<State, Trigger> phoneCall)
+        static void OnDutyNotDrivingOnExit()
         {
-            Console.WriteLine("[Status:] {0}", phoneCall);
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} OnDutyNotDrivingOnExit");
+        }
+
+        static void SleeperBerthgOnEntry(object value)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} SleeperBerthOnEntry {value}");
+        }
+
+        static void SleeperBerthgOnExit()
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} SleeperBerthOnExit");
+        }
+
+        static void OffDutyOnEntry(object value)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} OffDutyOnEntry {value}");
+        }
+
+        static void OffDutyOnExit()
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} OffDutyOnExit");
+        }
+
+        static void Fire<T>(StateMachine<State, Trigger> stateMachine, Trigger trigger, T value)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} Firing: {stateMachine.State} - {trigger} {value}");
+            try
+            {
+                if (stateMachine.CanFire(trigger))
+                {
+                    stateMachine.Fire(trigger, value);
+                }
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} {ex.Message}");
+            }
+
+        }
+
+        static void Print(StateMachine<State, Trigger> stateMachine)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString("yyyy/MM/dd-hh:mm:ss:fff")} Status: {stateMachine}");
         }
     }
 }
